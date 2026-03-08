d1e06a4c7969cac48258c070eb90283ddaaa64ef
ethan-prime <emh419@hotmail.com>
2026-03-08 18:58:57 -0500

    add ABI guard and CMake package consumer linkage checks

---
+++ .github/ci/consumer-cmake/consumer_c.c
@@ -0,0 +1,10 @@
+#include "generator.h"
+#include <stdint.h>
+
+int main(void)
+{
+    Generator g;
+    setupGenerator(&g, MC_1_20, 0);
+    applySeed(&g, DIM_OVERWORLD, (uint64_t)12345);
+    return getBiomeAt(&g, 1, 0, 63, 0) < 0;
+}

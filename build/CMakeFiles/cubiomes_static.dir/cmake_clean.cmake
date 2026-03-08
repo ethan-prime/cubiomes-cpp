3e82d62a8a35877d81f56e7b72f19d47c1ff166a
ethan-prime <emh419@hotmail.com>
2026-03-08 17:59:53 -0500

    ignore and untrack build artifacts

--- build/CMakeFiles/cubiomes_static.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcubiomes_static.a"
-  "libcubiomes_static.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang C)
-  include(CMakeFiles/cubiomes_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/cubiomes_static.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libcubiomes_static.a"
-)
